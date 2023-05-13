584993b717c3b6a4cbd538f3f2a908f015fa61bb
ttlekich <ttlekich@gmail.com>
2023-05-12 22:31:58 -0500

    fixing type

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,14 @@ import (
 	"github.com/ttlekich/ga/api"
 )
 
+type Thing struct {
+	a int
+}
+
+func add(b int) int {
+	return b
+}
+
 func main() {
 	port := flag.String("PORT", ":1337", "the server listening port")
 
--- pkg/hexgrid/hex.go
+++ pkg/hexgrid/hex.go
@@ -1,42 +1,47 @@
 package hexgrid
 
-type Hex struct {
+import "errors"
+
+type hex struct {
 	q int
 	r int
 	s int
 }
 
-func NewHex(q, r int) Hex {
-	return Hex{q: q, r: r, s: -q - r}
+func NewHex(q, r, s int) (*hex, error) {
+	if q+r+s != 0 {
+		return nil, errors.New("q + r + s must be 0")
+	}
+	return &hex{q: q, r: r, s: s}, nil
 }
 
 // Equality
-func Eq(a Hex, b Hex) bool {
+func Eq(a *hex, b *hex) bool {
 	return a.q == b.q && a.r == b.r && a.s == b.s
 }
 
-func NotEq(a Hex, b Hex) bool {
+func NotEq(a *hex, b *hex) bool {
 	return !Eq(a, b)
 }
 
 // Arithmetic
-func Add(a Hex, b Hex) Hex {
-	return NewHex(a.q+b.q, a.r+b.r)
+func Add(a *hex, b *hex) *hex {
+	return &hex{a.q + b.q, a.r + b.r, a.s + b.s}
 }
 
-func Subtract(a Hex, b Hex) Hex {
-	return NewHex(a.q-b.q, a.r-b.r)
+func Subtract(a *hex, b *hex) *hex {
+	return &hex{a.q - b.q, a.r - b.r, a.s - b.s}
 }
 
-func Multiply(a Hex, b Hex) Hex {
-	return NewHex(a.q*b.q, a.r*b.r)
+func Scale(a *hex, k int) *hex {
+	return &hex{a.q * k, a.r * k, a.s * k}
 }
 
 // Distance
-func Length(h Hex) int {
+func Length(h *hex) int {
 	return int((Abs(h.q) + Abs(h.r) + Abs(h.s)) / 2)
 }
 
-func Distance(a Hex, b Hex) int {
+func Distance(a *hex, b *hex) int {
 	return Length(Subtract(a, b))
 }
